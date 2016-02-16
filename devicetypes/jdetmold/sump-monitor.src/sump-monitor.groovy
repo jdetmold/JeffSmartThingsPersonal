0ec603ac3295995cb3b17829f8e98c97eac5dea9
jdetmold <jdetmold@Pro.local>
2016-02-16 00:18:16 -0700

    new

--- devicetypes/jdetmold/sump-monitor.src/sump-monitor.groovy
+++ devicetypes/jdetmold/sump-monitor.src/sump-monitor.groovy
@@ -12,6 +12,7 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
+ *  Use this at your own risk! I do not know what I am doing!
  */
 metadata {
 	definition (name: "Sump Monitor", namespace: "jdetmold", author: "jdetmold") {
